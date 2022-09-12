9b77ef1d79b5bc4006ffd4365d8a3fe164aa3996
cmderobertis <cmderobertis@gmail.com>
2022-09-11 19:30:48 -0700

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react"
+import reactLogo from "./assets/react.svg"
+import Chat from "./components/Chat"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <Chat />
+    </div>
+  )
+}
+
+export default App
